ad4783a5d8af427025dd6b82001cbb85fa5c6ebc
sisonj <98570174+sisonj3@users.noreply.github.com>
2025-04-29 16:59:25 -0700

    Changed slider to select

--- src/components/characterDisplay.jsx
+++ src/components/characterDisplay.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import getAgentMats from "../scripts/getAgentMats";
+import numArr from '../scripts/numArr';
 import plus from '../assets/plus.svg';
 import gear from '../assets/gear.svg';
 
@@ -282,7 +283,11 @@ export default function CharacterDisplay({ token, imgUrl, agent, index, updateCa
 
                     <div className="slider">
                         <label htmlFor="asc-c">Current Ascension:</label>
-                        <input type="range" name="asc-c" id="asc-c" min={0} max={5} value={ascC} onInput={changeAscC} />
+                        <select name="asc-c" id="asc-c">
+                            {numArr(0, 5).map((num, index) => (
+                                (num == ascC) ? <option key={index} value={num} selected>{ num }</option> : <option key={index} value={num}>{ num }</option>
+                            ))}
+                        </select>
                         <span>{ascC}</span>
                     </div>
 
---
+++ src/scripts/numArr.js
@@ -0,0 +1,9 @@
+export default function numArr(min, max) {
+    let arr = [];
+
+    for (let i = min; i <= max; i++) {
+        arr.push(i);
+    }
+
+    return arr;
+}
