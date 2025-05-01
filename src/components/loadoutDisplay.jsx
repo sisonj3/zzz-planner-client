76d1c599223e0fd161c94790df5a69a6cc9c16c7
sisonj <98570174+sisonj3@users.noreply.github.com>
2025-05-01 16:20:55 -0700

    Changed decimal to percentage display

--- src/components/loadoutDisplay.jsx
+++ src/components/loadoutDisplay.jsx
@@ -289,7 +289,7 @@ export default function LoadoutDisplay({ imgUrl, loadout, index, updateCallback,
                         </select>
                     </div>
                     
-                    <span className={highlight}>{((d1+d2+d3+d4+d5+d6) / 45).toFixed(2)}</span>
+                    <span className={highlight}>Effective Substats: {((d1+d2+d3+d4+d5+d6) / 45).toFixed(2) * 100}%</span>
                 </div>
 
                 <div className="loadouts hidden" ref={sliders} draggable onDragStart={preventDrag}>
