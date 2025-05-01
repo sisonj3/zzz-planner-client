54046091c9563975c309fd3a4bd8fbd4ea623ee8
sisonj <98570174+sisonj3@users.noreply.github.com>
2025-05-01 16:27:50 -0700

    Added styling to highlighted value

--- src/components/loadoutDisplay.jsx
+++ src/components/loadoutDisplay.jsx
@@ -289,7 +289,7 @@ export default function LoadoutDisplay({ imgUrl, loadout, index, updateCallback,
                         </select>
                     </div>
                     
-                    <div>Effective Substats: <span className={highlight}>{((d1+d2+d3+d4+d5+d6) / 45).toFixed(2) * 100}%</span></div>
+                    <div className="effectiveSub">Effective Substats: <span className={highlight}>{((d1+d2+d3+d4+d5+d6) / 45).toFixed(2) * 100}%</span></div>
                     
                 </div>
 
